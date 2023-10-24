9a864f918a73fd744784990b62c8257ca46713f0
zlibdev <enver.avisena@gmail.com>
2023-10-24 08:44:32 +0700

    DI-Patch fix

--- app/src/main/java/com/envr/idnsolo/dashboardislami/kajian/Model/KajianModel.kt
+++ app/src/main/java/com/envr/idnsolo/dashboardislami/kajian/Model/KajianModel.kt
@@ -1,4 +1,14 @@
 package com.envr.idnsolo.dashboardislami.kajian.Model
 
-class KajianModel {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class KajianModel(
+    var thumbnail : Int = 0,
+    var channel : String = "",
+    var speaker : String = "",
+    var title : String = "",
+    var urlVideo : String = "",
+    var summary : String = "",
+) : Parcelable
